73816a1cc227b83da3dcf81324d02b9e10b483bb
khanhnoi <nqkhanh1998@gmail.com>
2021-01-28 01:40:52 +0700

    done crud api

--- routes/api/post.js
+++ routes/api/post.js
@@ -19,6 +19,23 @@ router.get("/", async (req, res) => {
   }
 });
 
+//@routes GET api/posts
+//@desc Get an Post - Lấy mottj post
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    // console.log(id);
+    const post = await Posts.findById(id);
+    console.log(post);
+    if (!post) throw Error("No Item !!");
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(400).json({
+      message: error,
+    });
+  }
+});
+
 //@routes POST api/posts
 //@desc Create An Post - Thêm 1 post
 router.post("/", async (req, res) => {
@@ -30,6 +47,44 @@ router.post("/", async (req, res) => {
     const post = await newPost.save();
     if (!post) throw Error("Something went wrong while saving the post");
     res.status(200).json(post);
+  } catch (error) {
+    res.status(400).json({
+      message: error,
+    });
+  }
+});
+
+//@routes DELETE api/posts/:id
+//@desc Delete an Post - Xoá 1 post
+router.delete("/:id", async (req, res) => {
+  try {
+    console.log(req.params);
+    const id = req.params.id;
+    const posts = await Posts.findByIdAndDelete(id);
+    if (!posts) throw Error("No Post Found !!");
+    res.status(200).json({ success: true });
+  } catch (error) {
+    res.status(400).json({
+      message: error,
+    });
+  }
+});
+
+//@routes UPDATE api/posts/:id
+//@desc update an Post - Cập nhật 1 post
+router.patch("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    console.log(req.body);
+    const updateValue = req.body;
+    const posts = await Posts.findByIdAndUpdate(
+      id,
+      updateValue
+      //   { new: true },
+      //   () => console.log("- update post")
+    );
+    if (!posts) throw Error("Something went wrong while updating post !!");
+    res.status(200).json({ success: true });
   } catch (error) {
     res.status(400).json({
       message: error,
--- server.js
+++ server.js
@@ -15,7 +15,11 @@ app.use(express.json());
 //Connect to mongoDB
 // mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MONGODB connected"))
   .catch((error) => console.log(error));
 
